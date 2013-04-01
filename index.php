96e5fcc7f6f4674414e46bdc278d2b5a9ac3ca82
Patrik Affentranger <patrik.affentranger@gmail.com>
2013-04-01 19:29:34 +0800

    added body class and customised title of index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('perch/runtime.php'); ?><!DOCTYPE html>
 <html lang="en">
   <head>
-    <title><?php perch_pages_title(); ?></title>
+    <title><?php perch_pages_title(); ?> | <?php perch_content('Website Name'); ?></title>
     <meta charset="utf-8">
     <meta name="designer" content="Patrik Affentranger"><?php perch_content('SEO'); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1"><!--[if ! lt IE 6]>
@@ -13,12 +13,31 @@
     <link rel="apple-touch-icon" href="img/apple-touch-icon-114x114.png">
     <script type="text/javascript" src="js/lib/modernizr.min.js"></script>
   </head>
-  <body><!--[if lt IE 7]>
+  <body class="home"><!--[if lt IE 7]>
     <p class="chromeframe">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
     <header>
-      <nav role="navigation"></nav>
+      <nav role="navigation">
+        <?php
+            perch_pages_navigation(array(
+                'from-path'            => '/',
+                'levels'               => 0,
+                'hide-extensions'      => true,
+                'hide-default-doc'     => true,
+                'flat'                 => false,
+                'template'             => 'item.html',
+                'include-parent'       => false,
+                'skip-template'        => false,
+                'siblings'             => false,
+                'only-expand-selected' => false,
+                'add-trailing-slash'   => true,
+                'navgroup'             => false
+            ));
+        ?>
+      </nav>
     </header>
-    <section role="main"><?php perch_content('Intro'); ?></section>
+    <section role="main">
+      <h1><?php perch_content('Page Title'); ?></h1>
+    </section>
     <footer role="contentinfo">
       <p>&copy;</p>
     </footer>
